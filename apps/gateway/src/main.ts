6789183deb1d4e6d9315b266b401f031edf60c39
homow <homowsh897@gmail.com>
2026-08-12 19:49:50 +0330

    chore: add env package

--- apps/gateway/src/main.ts
+++ apps/gateway/src/main.ts
@@ -1,9 +1,15 @@
+import {resolve} from "path";
+import {config} from "dotenv";
 import {AppModule} from './app.module';
 import {NestFactory} from '@nestjs/core';
 
+config({path: resolve(process.cwd(), "apps/gateway/.env")});
+
+const PORT = Number(process.env.PORT || 3001);
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3001);
+  await app.listen(PORT);
 }
 
 bootstrap();
