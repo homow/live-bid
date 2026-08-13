2512c0c9b81f45141f51c392997988b5b4f2d4b2
homow <homowsh897@gmail.com>
2026-08-13 18:35:58 +0330

    chore: config redis for connection inside services

--- apps/gateway/src/app.module.ts
+++ apps/gateway/src/app.module.ts
@@ -2,6 +2,8 @@ import {Module} from '@nestjs/common';
 import {ClientsModule, Transport} from "@nestjs/microservices";
 import {AUTH_SERVICE_NAME} from "@live-bid/nestjs/services/names";
 
+console.log(process.env.NODE_ENV);
+
 @Module({
   imports: [
     ClientsModule.registerAsync([
@@ -9,7 +11,7 @@ import {AUTH_SERVICE_NAME} from "@live-bid/nestjs/services/names";
         name: AUTH_SERVICE_NAME,
         useFactory: () => ({
           transport: Transport.REDIS,
-          options: {}
+          options: {host: process.env.REDIS_HOST, port: Number(process.env.REDIS_PORT || 6973)}
         })
       }
     ]),
