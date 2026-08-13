f60582300903f947067fa9c6d5a62b2078647e4e
homow <homowsh897@gmail.com>
2026-08-13 18:31:39 +0330

    chore: add service name in contracts

--- apps/gateway/src/app.module.ts
+++ apps/gateway/src/app.module.ts
@@ -1,6 +1,18 @@
 import {Module} from '@nestjs/common';
+import {ClientsModule, Transport} from "@nestjs/microservices";
+import {AUTH_SERVICE_NAME} from "@live-bid/nestjs/services/names";
 
 @Module({
-  imports: [],
+  imports: [
+    ClientsModule.registerAsync([
+      {
+        name: AUTH_SERVICE_NAME,
+        useFactory: () => ({
+          transport: Transport.REDIS,
+          options: {}
+        })
+      }
+    ]),
+  ],
 })
 export class AppModule {}
