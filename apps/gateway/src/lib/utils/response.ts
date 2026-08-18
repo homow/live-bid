b0aeeae60a67dab89dd962197cf4ae0f644c6a49
homow <homowsh897@gmail.com>
2026-08-19 00:20:28 +0330

    refactor: clean codes

--- apps/gateway/src/lib/utils/response.ts
+++ apps/gateway/src/lib/utils/response.ts
@@ -1,25 +1,6 @@
 import z, {output} from 'zod';
 import {GraphQLError} from "graphql";
-import {HttpStatus} from "@nestjs/common";
 import type {ZodFieldError} from "@app/gateway/types";
-
-export const defaultMessages: Partial<Record<HttpStatus, string>> = {
-  [HttpStatus.OK]: 'Request Successful',
-  [HttpStatus.CREATED]: 'Resource Created',
-  [HttpStatus.NO_CONTENT]: 'Resource Deleted',
-  [HttpStatus.BAD_REQUEST]: 'Bad Request',
-  [HttpStatus.UNAUTHORIZED]: 'Unauthorized',
-  [HttpStatus.FORBIDDEN]: 'Forbidden',
-  [HttpStatus.NOT_FOUND]: 'Not Found',
-  [HttpStatus.CONFLICT]: 'Conflict',
-  [HttpStatus.TOO_MANY_REQUESTS]: "Too Many Requests",
-  [HttpStatus.INTERNAL_SERVER_ERROR]: 'Internal Server Error',
-};
-
-/** get Default message with status code */
-export function getDefaultMessage(status: HttpStatus): string {
-  return defaultMessages[status] || 'Unknown';
-}
 
 /** get structure format for zod errors */
 export function formatZodError(zodError: z.ZodError): ZodFieldError[] {
