de840771631ec5838f1afe6424197294183caccf
homow <homowsh897@gmail.com>
2026-08-19 00:17:53 +0330

    refactor: delete unused types

--- apps/gateway/src/types/response.types.ts
+++ apps/gateway/src/types/response.types.ts
@@ -1,33 +1,5 @@
-/** base response for exceptions and ok responses */
-export interface BaseResponse {
-  success: boolean;
-  statusCode: number;
-  detail: string;
-  timestamp: string;
-  path?: string;
-}
-
 /** params type for get schema for swagger when zod validate not success */
 export interface ZodFieldError {
   field: string;
   error: string;
 }
-
-/** base error type */
-export interface BaseException {
-  message: string;
-  error: string;
-}
-
-/** type of zod error response */
-export interface ZodException {
-  message: string;
-  errors: ZodFieldError[];
-}
-
-/** base exception response */
-export type ZodExceptionRes = & BaseResponse & ZodException;
-
-export type BaseExceptionRes = & BaseResponse & BaseException & {
-  [key: string]: unknown;
-};
