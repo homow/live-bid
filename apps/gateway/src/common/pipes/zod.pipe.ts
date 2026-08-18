84784145c02d6266dbcf2b675421ce3cee79d7a1
homow <homowsh897@gmail.com>
2026-08-19 00:05:16 +0330

    chore: add pipe for zod

---
+++ apps/gateway/src/common/pipes/zod.pipe.ts
@@ -0,0 +1,12 @@
+import z from "zod";
+import {checkZod} from "@app/gateway/lib";
+import {Injectable, PipeTransform} from '@nestjs/common';
+
+@Injectable()
+export class ZodPipe<T extends z.ZodTypeAny> implements PipeTransform {
+  constructor(private readonly schema: T) {}
+
+  transform(value: unknown): z.infer<T> {
+    return checkZod(this.schema, value);
+  }
+}
