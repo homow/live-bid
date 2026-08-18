47054c6b9debef20898d5cc43c5e6c1702ecb6e8
homow <homowsh897@gmail.com>
2026-08-19 01:04:29 +0330

    chore: add user validator

---
+++ packages/contracts/src/shared/dto/auth/register.dto.ts
@@ -0,0 +1,34 @@
+import {z} from "zod";
+
+export const DisplayUserNameSchema = z.string().min(3).max(60).trim();
+
+export const UserRegisterValidator = z.object({
+  username: z.string()
+    .trim()
+    .min(3, "Username must be at least 3 characters")
+    .max(30, "Username must be at most 30 characters")
+    .regex(
+      /^[a-zA-Z0-9_]+$/,
+      "Username can only contain letters, numbers, and underscore (_)"
+    )
+    .regex(
+      /^[a-zA-Z]/,
+      "Username must start with a letter"
+    )
+    .regex(
+      /^[a-zA-Z0-9_]*$/,
+      "Username can only contain letters, numbers, and underscore"
+    ),
+  display_name: DisplayUserNameSchema,
+  password: z.string()
+    .trim()
+    .min(8)
+    .max(25)
+    .regex(
+      /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d!@#$%^&*]+$/,
+      {error: "password must contain at least one letter and one number"}
+    ),
+  email: z.email().nonempty().trim()
+});
+
+export type RegisterRegisterValidatorType = z.infer<typeof DisplayUserNameSchema>;
