d8897dea4178a83dc8d053d6e924e17a0338caef
homow <homowsh897@gmail.com>
2026-08-18 21:53:59 +0330

    refactor: microservice client proxy configs

--- apps/gateway/src/app.module.ts
+++ apps/gateway/src/app.module.ts
@@ -1,11 +1,9 @@
 import {Module} from '@nestjs/common';
+import {ConfigModule} from "@nestjs/config";
 import {GraphQLModule} from "@nestjs/graphql";
 import {graphqlConfigs} from "@app/gateway/lib";
 import {ApolloDriverConfig} from "@nestjs/apollo";
-import {AppResolver} from "@app/gateway/app.resolver";
-import {ConfigModule, ConfigService} from "@nestjs/config";
-import {ClientsModule, Transport} from "@nestjs/microservices";
-import {AUTH_SERVICE_NAME, CORE_SERVICE_NAME} from "@live-bid/contracts/services/names";
+import {AppGraphQLModule} from "@app/gateway/graphql/graphql.module";
 
 @Module({
   imports: [
@@ -14,42 +12,12 @@ import {AUTH_SERVICE_NAME, CORE_SERVICE_NAME} from "@live-bid/contracts/services
       isGlobal: true,
       envFilePath: "apps/gateway/.env",
     }),
+
+    // GraphQL Configs
     GraphQLModule.forRoot<ApolloDriverConfig>(graphqlConfigs),
 
-    // Client Services
-    ClientsModule.registerAsync([
-      {
-        name: AUTH_SERVICE_NAME,
-        useFactory: (config: ConfigService) => ({
-          transport: Transport.REDIS,
-          options: {
-            host: config.get<string>("REDIS_HOST"),
-            port: Number(config.get<string>("REDIS_PORT") || 6973),
-            retryAttempts: 5,
-            retryDelay: 1000,
-            retryStrategy: () => 1000
-          },
-        }),
-        inject: [ConfigService]
-      },
-      {
-        name: CORE_SERVICE_NAME,
-        useFactory: (config: ConfigService) => ({
-          transport: Transport.REDIS,
-          options: {
-            host: config.get<string>("REDIS_HOST"),
-            port: Number(config.get<string>("REDIS_PORT") || 6973),
-            retryAttempts: 5,
-            retryDelay: 1000,
-            retryStrategy: () => 1000
-          },
-        }),
-        inject: [ConfigService]
-      }
-    ]),
+    // App GraphQL Module
+    AppGraphQLModule,
   ],
-  providers: [
-    AppResolver,
-  ]
 })
 export class AppModule {}
--- apps/gateway/src/graphql/graphql.module.ts
+++ apps/gateway/src/graphql/graphql.module.ts
@@ -1,9 +1,45 @@
 import {Module} from "@nestjs/common";
 import * as Resolvers from "./resolvers";
+import {ConfigService} from "@nestjs/config";
+import {ClientsModule, Transport} from "@nestjs/microservices";
+import {AUTH_SERVICE_NAME, CORE_SERVICE_NAME} from "@live-bid/contracts/services/names";
 
 @Module({
+  imports: [
+    // Client Services
+    ClientsModule.registerAsync([
+      {
+        name: AUTH_SERVICE_NAME,
+        useFactory: (config: ConfigService) => ({
+          transport: Transport.REDIS,
+          options: {
+            host: config.get<string>("REDIS_HOST"),
+            port: Number(config.get<string>("REDIS_PORT") || 6973),
+            retryAttempts: 5,
+            retryDelay: 1000,
+            retryStrategy: () => 1000
+          },
+        }),
+        inject: [ConfigService]
+      },
+      {
+        name: CORE_SERVICE_NAME,
+        useFactory: (config: ConfigService) => ({
+          transport: Transport.REDIS,
+          options: {
+            host: config.get<string>("REDIS_HOST"),
+            port: Number(config.get<string>("REDIS_PORT") || 6973),
+            retryAttempts: 5,
+            retryDelay: 1000,
+            retryStrategy: () => 1000
+          },
+        }),
+        inject: [ConfigService]
+      }
+    ]),
+  ],
   providers: [
     Resolvers.AuthResolver
-  ]
+  ],
 })
-export class GraphQLModule {}
+export class AppGraphQLModule {}
