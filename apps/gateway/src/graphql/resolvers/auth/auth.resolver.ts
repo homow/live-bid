9e03d3460ddd6dfeb8ad25132c63c983358e42c0
homow <homowsh897@gmail.com>
2026-08-19 20:44:33 +0330

    chore: get input from resolver

--- apps/gateway/src/graphql/resolvers/auth/auth.resolver.ts
+++ apps/gateway/src/graphql/resolvers/auth/auth.resolver.ts
@@ -1,9 +1,12 @@
 import {firstValueFrom} from "rxjs";
-import {Inject} from "@nestjs/common";
-import {Resolver, Query} from "@nestjs/graphql";
 import {ClientProxy} from "@nestjs/microservices";
-import {AUTH_SERVICE_NAME} from "@live-bid/services/names";
+import {Inject} from "@nestjs/common";
+import {Resolver, Mutation, Args} from "@nestjs/graphql";
 import * as Messages from "@live-bid/services/graphql-messages";
+import {AUTH_SERVICE_NAME} from "@live-bid/services/names";
+import * as AuthInputs from "./inputs";
+import * as AuthSchemas from "@live-bid/contracts/schemas/auth";
+import {ZodPipe} from "@app/gateway/common";
 
 @Resolver()
 export class AuthResolver {
@@ -11,8 +14,16 @@ export class AuthResolver {
     @Inject(AUTH_SERVICE_NAME) private readonly authClient: ClientProxy
   ) {}
 
-  @Query(() => String)
-  register() {
+  @Mutation(() => String)
+  register(
+    @Args(
+      "input",
+      {type: () => AuthInputs.RegisterUserInput},
+      new ZodPipe(AuthSchemas.RegisterUserSchema)
+    )
+    input: AuthSchemas.RegisterUserSchemaType
+  ) {
+    console.log(input);
     return firstValueFrom(this.authClient.send(Messages.AUTH_MESSAGES.REGISTER, {}));
   }
 }
