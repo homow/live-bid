4a36b2f32cfb7dc41564a1231ef7b046959a49ae
homow <homowsh897@gmail.com>
2026-08-19 20:29:12 +0330

    refactor: zod pipe

--- apps/gateway/src/common/pipes/zod.pipe.ts
+++ apps/gateway/src/common/pipes/zod.pipe.ts
@@ -1,5 +1,6 @@
 import z from "zod";
-import {checkZod} from "@app/gateway/lib";
+import {GraphQLError} from "graphql";
+import type {ZodFieldError} from "@app/gateway/types";
 import {Injectable, PipeTransform} from '@nestjs/common';
 
 @Injectable()
@@ -7,6 +8,25 @@ export class ZodPipe<T extends z.ZodTypeAny> implements PipeTransform {
   constructor(private readonly schema: T) {}
 
   transform(value: unknown): z.infer<T> {
-    return checkZod(this.schema, value);
+    const result = this.schema.safeParse(value);
+
+    if (!result.success) {
+      throw new GraphQLError("Invalid Request.", {
+        extensions: {
+          errors: this.formatZodError(result.error),
+          code: "BAD_REQUEST",
+        }
+      });
+    }
+
+    return result.data;
+  }
+
+  /** get structure format for zod errors */
+  formatZodError(zodError: z.ZodError): ZodFieldError[] {
+    return zodError?.issues?.map(i => ({
+      field: i.path.join(", "),
+      error: i.message,
+    }));
   }
 }
