1f69d47bb2d2bf319c872def6bc036cfc4f86435
homow <homowsh897@gmail.com>
2026-08-20 19:22:56 +0330

    chore: add role to user schema

--- packages/contracts/src/schemas/auth/register.schema.ts
+++ packages/contracts/src/schemas/auth/register.schema.ts
@@ -3,14 +3,14 @@ import {z} from "zod";
 export const UserDisplayNameSchema = z.string().min(3).max(60).trim();
 
 export const RegisterUserSchema = z.object({
-  username: z.string()
-    .trim()
-    .min(3, "Username must be at least 3 characters")
-    .max(30, "Username must be at most 30 characters")
-    .regex(
-      /^[a-zA-Z][a-zA-Z0-9_]*$/,
-      "Username must start with a letter and can only contain letters, numbers, and underscore"
-    ),
+  // username: z.string()
+  //   .trim()
+  //   .min(3, "Username must be at least 3 characters")
+  //   .max(30, "Username must be at most 30 characters")
+  //   .regex(
+  //     /^[a-zA-Z][a-zA-Z0-9_]*$/,
+  //     "Username must start with a letter and can only contain letters, numbers, and underscore"
+  //   ),
   display_name: UserDisplayNameSchema,
   password: z.string()
     .trim()
--- packages/services/src/database/schema/user.schema.ts
+++ packages/services/src/database/schema/user.schema.ts
@@ -1,13 +1,17 @@
+import {UserRoleEnum} from "../../lib";
 import {baseDrizzleTable} from "./common";
-import {boolean, pgTable, varchar} from "drizzle-orm/pg-core";
+import {boolean, pgEnum, pgTable, varchar} from "drizzle-orm/pg-core";
+
+const userRoleEnum = pgEnum('user_role', UserRoleEnum);
 
 export const user = pgTable("users", {
   ...baseDrizzleTable,
   password: varchar("password", {length: 255}).notNull(),
   email: varchar("email", {length: 13}).notNull().unique(),
-  username: varchar("username", {length: 255}).notNull().unique(),
+  username: varchar("username", {length: 255}).unique(),
   isActive: boolean("is_active").default(true).notNull(),
   display_name: varchar("display_name", {length: 60}).notNull(),
+  role: userRoleEnum('role').notNull().default(UserRoleEnum.USER),
 });
 
 export type User = typeof user.$inferSelect;
