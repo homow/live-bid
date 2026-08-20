c7beaf02b06d5ee6cea0dcfc657f9b579b7ff0d9
homow <homowsh897@gmail.com>
2026-08-20 07:08:25 +0330

    chore: add drizzle module to auth service

--- apps/auth/src/app.module.ts
+++ apps/auth/src/app.module.ts
@@ -1,6 +1,8 @@
 import * as Modules from "./modules";
 import {Module} from '@nestjs/common';
 import {ConfigModule} from "@nestjs/config";
+import {DrizzleModule} from "@live-bid/services/database";
+import * as drizzleSchemas from "@live-bid/services/database/schema";
 
 @Module({
   imports: [
@@ -8,6 +10,8 @@ import {ConfigModule} from "@nestjs/config";
       isGlobal: true,
       envFilePath: "apps/auth/.env",
     }),
+
+    DrizzleModule.forRoot(drizzleSchemas),
 
     // App Modules
     Modules.AuthModule,
