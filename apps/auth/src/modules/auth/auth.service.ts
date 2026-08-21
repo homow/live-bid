1c2df328f9debf312932ac258d492101f3847704
homow <homowsh897@gmail.com>
2026-08-21 22:55:45 +0330

    chore: add log to register

--- apps/auth/src/modules/auth/auth.service.ts
+++ apps/auth/src/modules/auth/auth.service.ts
@@ -1,3 +1,4 @@
+import {PinoLogger} from "nestjs-pino";
 import {hashSecret} from "@app/auth/lib";
 import {Injectable} from "@nestjs/common";
 import {AuthRepository} from "./auth.repository";
@@ -7,17 +8,27 @@ import {UserRepository} from "@app/auth/modules/user/user.repository";
 @Injectable()
 export class AuthService {
   constructor(
+    private readonly logger: PinoLogger,
     private readonly authRepository: AuthRepository,
     private readonly userRepository: UserRepository,
-  ) {}
+  ) {
+    this.logger.setContext(AuthService.name);
+  }
 
   /** **Register User** */
   async register(userData: Schemas.RegisterUserSchemaType) {
     const hashedPassword: string = await hashSecret(userData.password);
 
-    return this.userRepository.createUser({
+    const user = await this.userRepository.createUser({
       ...userData,
       password: hashedPassword,
     });
+
+    this.logger.info(
+      {userId: user.id, display_name: user.display_name},
+      "User registered successfully",
+    );
+
+    return user;
   }
 }
