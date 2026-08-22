381d48aa2f9d2a464b2c1bcf3fe0cf7f818b4fe8
homow <homowsh897@gmail.com>
2026-08-22 20:17:41 +0330

    refactor(contracts): separate common schemas and restructure auth folder for zod

--- packages/contracts/src/schemas/auth/register.schema.ts
+++ packages/contracts/src/schemas/auth/register.schema.ts
@@ -1,25 +1,9 @@
 import {z} from "zod";
-
-export const UserDisplayNameSchema = z.string().min(3).max(60).trim();
+import {UserPasswordSchema, UserDisplayNameSchema} from "../common";
 
 export const RegisterUserSchema = z.object({
-  // username: z.string()
-  //   .trim()
-  //   .min(3, "Username must be at least 3 characters")
-  //   .max(30, "Username must be at most 30 characters")
-  //   .regex(
-  //     /^[a-zA-Z][a-zA-Z0-9_]*$/,
-  //     "Username must start with a letter and can only contain letters, numbers, and underscore"
-  //   ),
   display_name: UserDisplayNameSchema,
-  password: z.string()
-    .trim()
-    .min(8)
-    .max(25)
-    .regex(
-      /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d!@#$%^&*]+$/,
-      {error: "password must contain at least one letter and one number"}
-    ),
+  password: UserPasswordSchema,
   email: z.email().nonempty().trim()
 });
 
