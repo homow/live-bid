529d746b3268dc6a7e5bfdf5ce33157026976008
homow <homowsh897@gmail.com>
2026-08-23 05:51:14 +0330

    feat(util): add new for get client info

--- apps/gateway/src/lib/utils/index.ts
+++ apps/gateway/src/lib/utils/index.ts
@@ -1,2 +1,3 @@
 export * from "./time";
 export * from "./outputs";
+export * from "./normalize-client-info";
---
+++ apps/gateway/src/lib/utils/normalize-client-info.ts
@@ -0,0 +1,15 @@
+import type {NormalizedClientInfo} from "@live-bid/services/types";
+
+export function normalizeClientInfo(input: Express.Request["clientInfo"]): NormalizedClientInfo {
+  const {ip, ua, geo, lang} = input;
+
+  return {
+    ip,
+    browser: ua.browser?.name ?? null,
+    os: ua.os?.name ?? null,
+    device: ua.device?.type ?? "desktop",
+    country: geo?.country ?? null,
+    city: geo?.city ?? null,
+    lang,
+  };
+}
