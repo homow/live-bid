ef8878bea7056056d6d2dff344b48119e1329e0f
homow <homowsh897@gmail.com>
2026-08-24 11:57:00 +0330

    feat(service): get user and check password in login method

--- apps/auth/src/modules/auth/auth.service.ts
+++ apps/auth/src/modules/auth/auth.service.ts
@@ -1,10 +1,11 @@
 import {PinoLogger} from "nestjs-pino";
-import {hashSecret} from "@app/auth/lib";
+import {compareSecret, hashSecret} from "@app/auth/lib";
 import {Injectable} from "@nestjs/common";
 import {AuthRepository} from "./auth.repository";
 import * as Schemas from "@live-bid/contracts/schemas";
 import {NormalizeClientInfoType} from "@live-bid/services/types";
 import {UserRepository} from "@app/auth/modules/user/user.repository";
+import {AppException} from "@live-bid/services/lib";
 
 @Injectable()
 export class AuthService {
@@ -34,6 +35,26 @@ export class AuthService {
   }
 
   async login(userData: Schemas.LoginUserSchemaType, clientInfo: NormalizeClientInfoType) {
-    
+    const user = await this.userRepository.findOne(
+      {
+        email: userData.email,
+        username: userData.username,
+      },
+      false
+    );
+
+    if (!user) throw new AppException({
+      statusCode: 404,
+      code: "User not found",
+      message: "User does not exist in database, please check phone and try again",
+    });
+
+    const isValidPassword = await compareSecret(userData.password, user.password);
+
+    if (!isValidPassword) throw new AppException({
+      statusCode: 401,
+      code: 'Invalid Credentials',
+      message: "Invalid user credentials",
+    });
   }
 }
