7498985cf3d566d8f3fe6e03cac16d38989f7cb1
homow <homowsh897@gmail.com>
2026-08-24 21:20:08 +0330

    refactor(style)

--- apps/gateway/src/graphql/resolvers/auth/auth.resolver.ts
+++ apps/gateway/src/graphql/resolvers/auth/auth.resolver.ts
@@ -9,7 +9,7 @@ import {NormalizeClientInfo, ZodPipe} from "@app/gateway/common";
 import {Resolver, Mutation, Args, Context} from "@nestjs/graphql";
 import * as GraphqlMessages from "@live-bid/services/graphql-messages";
 import {ACCESS_TOKEN_NAME, AUTH_SERVICE_NAME, REFRESH_TOKEN_NAME} from "@live-bid/services/names";
-import type {LoginRequest, LoginResponse, NormalizeClientInfoType} from "@live-bid/services/types";
+import type {LoginRequest, LoginResponse, NormalizeClientInfoType, SafeUser} from "@live-bid/services/types";
 
 @Resolver()
 export class AuthResolver {
@@ -43,7 +43,7 @@ export class AuthResolver {
     ) input: ZodSchemas.LoginUserSchemaType,
     @Context() context: GraphQLContext,
     @NormalizeClientInfo() clientInfo: NormalizeClientInfoType
-  ) {
+  ): Promise<SafeUser> {
     const result = await firstValueFrom<LoginResponse>(this.authClient.send(GraphqlMessages.AUTH_MESSAGES.LOGIN, {
       clientInfo,
       userData: input
