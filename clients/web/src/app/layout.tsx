e160e16d0ce48397b71d1e77125b6b96e3029d69
niyayesh <niyayeshammardii@gmail.com>
2026-08-25 12:32:51 -0700

    add Footer to section layout

--- clients/web/src/app/layout.tsx
+++ clients/web/src/app/layout.tsx
@@ -1,7 +1,8 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { ReactNode } from "react";
-import Header from "@/featuer/Home/Header";
+import Header from "@/Components/layout/Header";
+import Footer from "@/Components/layout/Footer";
 
 export const metadata: Metadata = {
   title: "live bed",
@@ -13,8 +14,8 @@ export default function RootLayout({ children }: { children: ReactNode }) {
     <html lang="en" className={` h-full antialiased`}>
       <body>
         <Header />
-
         <main className="flex-1">{children}</main>
+        <Footer />
       </body>
     </html>
   );
