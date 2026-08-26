d66e843e79a4dc52fa4445e29cc5e92e353d9711
homow <homowsh897@gmail.com>
2026-08-26 16:21:48 +0330

    feat(pattern): add new pattern message for validate refresh token

--- packages/services/src/messages/auth.messages.ts
+++ packages/services/src/messages/auth.messages.ts
@@ -1,4 +1,5 @@
-export const AUTH_MESSAGES = {
+export const AUTH_PATTERNS = {
   REGISTER: "auth.register",
   LOGIN: "auth.login",
+  VALIDATE_REFRESH_TOKEN: "auth.vaildate_refresh_token",
 } as const;
