c66719f55a4b8e0dfbb281644a73d2b999de96ff
homow <homowsh897@gmail.com>
2026-08-27 17:16:47 +0330

    refactor(route): make public healthcheck

--- apps/gateway/src/graphql/resolvers/health/health.resolver.ts
+++ apps/gateway/src/graphql/resolvers/health/health.resolver.ts
@@ -1,13 +1,16 @@
+import {Public} from "@app/gateway/common";
 import {HealthCheckOutput} from './outputs';
 import {Query, Resolver} from '@nestjs/graphql';
 
 @Resolver()
 export class HealthResolver {
+  @Public()
   @Query(() => String)
   health(): string {
     return 'OK';
   }
 
+  @Public()
   @Query(() => HealthCheckOutput)
   healthCheck(): HealthCheckOutput {
     return {
