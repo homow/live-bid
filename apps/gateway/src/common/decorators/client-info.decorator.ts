dddb020689055f172fe6cc44ed18a4167b59bdb3
homow <homowsh897@gmail.com>
2026-08-28 19:43:20 +0330

    refactor(style): imports

--- apps/gateway/src/common/decorators/client-info.decorator.ts
+++ apps/gateway/src/common/decorators/client-info.decorator.ts
@@ -1,9 +1,9 @@
+import geoip from "geoip-lite";
+import {UAParser} from "ua-parser-js";
 import {GraphQLContext} from "@app/gateway/types";
 import {GqlExecutionContext} from "@nestjs/graphql";
 import {normalizeClientInfo} from "@app/gateway/lib";
 import {createParamDecorator, ExecutionContext} from "@nestjs/common";
-import {UAParser} from "ua-parser-js";
-import geoip from "geoip-lite";
 
 export const NormalizeClientInfo = createParamDecorator(
   (_data: never, ctx: ExecutionContext) => {
