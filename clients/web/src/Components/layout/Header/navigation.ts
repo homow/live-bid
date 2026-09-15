ef0f64427e40e0272e484e9ea2ef1d36820637f8
niyayesh <niyayeshammardii@gmail.com>
2026-09-15 08:39:21 -0700

    creat navigation to header

---
+++ clients/web/src/Components/layout/Header/navigation.ts
@@ -0,0 +1,24 @@
+import { CircleHelp, Gavel, Home, Trophy } from "lucide-react";
+
+export const navigationItems = [
+  {
+    label: "Home",
+    href: "/",
+    icon: Home,
+  },
+  {
+    label: "Auctions",
+    href: "/auctions",
+    icon: Gavel,
+  },
+  {
+    label: "Winners",
+    href: "/winners",
+    icon: Trophy,
+  },
+  {
+    label: "How It Works",
+    href: "/How-to-Work",
+    icon: CircleHelp,
+  },
+];
