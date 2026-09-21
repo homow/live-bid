c3c204df2e343e49bba93ddcf336fa29754b4f8c
niyayesh <niyayeshammardii@gmail.com>
2026-09-21 08:32:55 -0700

    creat dashborad slidbar

---
+++ clients/web/src/featuer/Dashboard/DashboardSlidbar.tsx
@@ -0,0 +1,24 @@
+import { Settings, Heart, Wallet, Trophy } from "lucide-react";
+
+export const navigationItems = [
+  {
+    label: "Settings",
+    href: "/dashboard/setting",
+    icon: Settings,
+  },
+  {
+    label: "Favorite",
+    href: "/dashboard/favorite",
+    icon: Heart,
+  },
+  {
+    label: "Wallet",
+    href: "/dashboard/wallet",
+    icon: Wallet,
+  },
+  {
+    label: "Won Auctions",
+    href: "/dashboard/won-auctions",
+    icon: Trophy,
+  },
+];
