c517048c8953d03541060826abbeedf34403aaf4
niyayesh <niyayeshammardii@gmail.com>
2026-09-21 10:33:19 -0700

    cut header and footer to main hayout

--- clients/web/src/app/layout.tsx
+++ clients/web/src/app/layout.tsx
@@ -1,12 +1,10 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { ReactNode } from "react";
-import Header from "@/Components/layout/Header/Header";
-import Footer from "@/Components/layout/Footer/Footer";
 import { Geist } from "next/font/google";
 import { cn } from "@/lib/utils";
 
-const geist = Geist({subsets:['latin'],variable:'--font-sans'});
+const geist = Geist({ subsets: ["latin"], variable: "--font-sans" });
 
 export const metadata: Metadata = {
   title: "live bed",
@@ -15,11 +13,12 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en" className={cn("h-full", "antialiased", "font-sans", geist.variable)}>
+    <html
+      lang="en"
+      className={cn("h-full", "antialiased", "font-sans", geist.variable)}
+    >
       <body>
-        <Header />
         <main className="flex-1">{children}</main>
-        <Footer />
       </body>
     </html>
   );
