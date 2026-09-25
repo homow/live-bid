ab92f435796b6696562c8d205d70ddf0af4c6210
niyayesh <niyayeshammardii@gmail.com>
2026-09-25 07:41:05 -0700

    add rout setting in dashboard

---
+++ clients/web/src/app/(dashboard)/dashboard/setting/page.tsx
@@ -0,0 +1,13 @@
+
+import Settings from "@/featuer/Dashboard/DashboardSetting/Settings";
+const Setting =()=>  {
+
+        return (
+            <div>
+            <Settings/>
+            </div>
+        );
+    }
+
+
+export default Setting;
